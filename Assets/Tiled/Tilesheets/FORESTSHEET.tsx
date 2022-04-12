d01d903f9ec94a3c20cd7b5dc5ac7f6b2c4d6a73
Minionade <trevorbarrett137@gmail.com>
2022-04-12 02:48:06 -0400

    yes
    indoubtedly

---
+++ Assets/Tiled/Tilesheets/FORESTSHEET.tsx
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="FORESTSHEET" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../TestingStuff/FORESTSHEET.png" width="128" height="128"/>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="15.9915" y="10.9729">
+    <polygon points="0,0 0.0111111,-3.88868 -12.9718,-11.0154 -15.949,-11.0154 -15.949,-6.97501 -13.0144,-7.01754"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.84636" y="15.949">
+    <polygon points="0,0 7.14514,0.0425306 -1.81044,-4.82396 -8.82969,-8.87642 -8.84636,-4.168"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0.994097,8.67362e-18 16,16 11.9778,16 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
