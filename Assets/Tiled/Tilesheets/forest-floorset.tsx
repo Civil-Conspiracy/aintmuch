bdd8d84365a8c3b95c1a99883fff650816c1be2c
Minionade <trevorbarrett137@gmail.com>
2022-04-14 00:40:26 -0400

    yes

--- Assets/Tiled/Tilesheets/forest-floorset.tsx
+++ Assets/Tiled/Tilesheets/forest-floorset.tsx
@@ -1,4 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.4" name="forest-floorset" tilewidth="16" tileheight="16" tilecount="16" columns="4">
  <image source="../../Sprites/World/forest-floorset.png" width="64" height="64"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="-0.0869565" y="0">
+    <polygon points="0.0625,0 5.09443,-0.00339674 5.0873,4.99762 0.09375,4.99321"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="0.375" y="0.5">
+    <polygon points="-0.4,-0.511111 15.663,-0.407609 15.6793,6.54348 -0.355556,6.47778"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="15.9064" y="-0.0425306">
+    <polygon points="-11.8694,4.01069 -15.9064,0.0350611 -15.9064,16.0398 -8.8861,16.0451 -8.9,7.03977 -8.93896,6.99049"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
